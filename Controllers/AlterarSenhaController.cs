57f40766d2fcbc7898ca07a7ebe960e7866d88d4
gabrielvn95 <gabrielsantossim.nogueira4@gmail.com>
2025-05-22 10:41:53 -0300

    Add new AlterarSenhaController

---
+++ Controllers/AlterarSenhaController.cs
@@ -0,0 +1,47 @@
+﻿using GestVeicular.Models;
+using GestVeicular.Repositorio;
+using GestVeicular.Services.SessaoService;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GestVeicular.Controllers
+{
+    public class AlterarSenhaController : Controller
+    {
+        private readonly IUsuarioRepositorio _usuarioRepositorio;
+        private readonly ISessaoInterface _sessaoInterface;
+        public AlterarSenhaController(IUsuarioRepositorio usuarioRepositorio, ISessaoInterface sessaoInterface)
+        {
+                _sessaoInterface = sessaoInterface;
+                _usuarioRepositorio = usuarioRepositorio;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Alterar(AlterarSenha alterarSenha)
+        {
+            try
+            {
+                var usuario = _sessaoInterface.BuscarSessao();
+                alterarSenha.Id = usuario.IdUsuario;
+
+                if (ModelState.IsValid)
+                {
+                    _usuarioRepositorio.AlterarSenha(alterarSenha);
+                    TempData["MensagemSucesso"] = "Senha alterada com sucesso!";
+                    return RedirectToAction("Index");
+                }
+                return View("Index", alterarSenha);
+
+            }
+            catch (Exception ex)
+            {
+                TempData["MensagemErro"] = "Erro ao alterar a senha: " + ex.Message;
+                return RedirectToAction("Index");
+            }
+
+        }
+    }
+}
