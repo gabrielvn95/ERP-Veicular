6a320b3b6f2d9cd9b30b36c3fc000a1b122de902
gabrielvn95 <gabrielsantossim.nogueira4@gmail.com>
2025-05-28 19:51:58 -0300

    Add new view PainelAdmin is dashbord for control all user

---
+++ Controllers/UsuarioController.cs
@@ -0,0 +1,74 @@
+﻿using GestVeicular.Enums;
+using GestVeicular.Models;
+using GestVeicular.Repositorio;
+using GestVeicular.Services.SessaoService;
+using Microsoft.AspNetCore.Mvc;
+
+public class UsuarioController : Controller
+{
+    private readonly IUsuarioRepositorio _usuarioRepositorio;
+    private readonly ISessaoInterface _sessaoService;
+
+    public UsuarioController(IUsuarioRepositorio usuarioRepositorio, ISessaoInterface sessaoService)
+    {
+        _usuarioRepositorio = usuarioRepositorio;
+        _sessaoService = sessaoService;
+    }
+
+    private Usuario ObterUsuarioLogado()
+    {
+        return _sessaoService.BuscarSessao();
+    }
+
+    public IActionResult PainelAdmin()
+    {
+        var usuarioLogado = ObterUsuarioLogado();
+
+        if (usuarioLogado == null || usuarioLogado.TipoUsuario != TipoUsuario.Admin)
+        {
+            TempData["MensagemErro"] = "Acesso negado: você não tem permissão para acessar essa página.";
+            return RedirectToAction("Index", "Home");
+        }
+
+        var usuarios = _usuarioRepositorio.BuscarTodosUsuarios();
+        return View(usuarios);
+    }
+
+    public IActionResult Editar(int id)
+    {
+        var usuario = _usuarioRepositorio.BuscarPorId(id);
+        if (usuario == null) return NotFound();
+        return View(usuario);
+    }
+
+    [HttpPost]
+    public IActionResult Editar(Usuario usuario)
+    {
+        try
+        {
+            _usuarioRepositorio.Atualizar(usuario);
+            TempData["MensagemSucesso"] = "Usuário atualizado com sucesso!";
+            return RedirectToAction("PainelAdmin");
+        }
+        catch (Exception ex)
+        {
+            TempData["MensagemErro"] = $"Erro ao atualizar usuário: {ex.Message}";
+            return View(usuario);
+        }
+    }
+
+    public IActionResult Deletar(int id)
+    {
+        try
+        {
+            _usuarioRepositorio.Apagar(id);
+            TempData["MensagemSucesso"] = "Usuário removido com sucesso!";
+        }
+        catch (Exception ex)
+        {
+            TempData["MensagemErro"] = $"Erro ao remover usuário: {ex.Message}";
+        }
+
+        return RedirectToAction("PainelAdmin");
+    }
+}
--- obj/Debug/net8.0/GestVeicular.AssemblyInfo.cs
+++ obj/Debug/net8.0/GestVeicular.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GestVeicular")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3c9828bc53d148cba00bbc748846c8d996ca086")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+204d4339cf3526eec7a2f8a75c55c24f3f1ff74c")]
 [assembly: System.Reflection.AssemblyProductAttribute("GestVeicular")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GestVeicular")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
