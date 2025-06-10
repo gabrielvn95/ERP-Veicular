46c262972db1d0dcdefca449b3e692e053cb5dcc
gabrielvn95 <gabrielsantossim.nogueira4@gmail.com>
2025-06-10 19:29:25 -0300

    Add view Deletar

--- obj/Debug/net8.0/GestVeicular.AssemblyInfo.cs
+++ obj/Debug/net8.0/GestVeicular.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GestVeicular")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c616836a060bcda1e2b918cdaee035ac528d65fb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ee56f62fd87e1bd969ecea1e85238b80a3db7ad")]
 [assembly: System.Reflection.AssemblyProductAttribute("GestVeicular")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GestVeicular")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
